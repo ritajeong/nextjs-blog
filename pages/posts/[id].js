2401c859d2b5a9029a9eb6e7b3a06d11787656b9
ritajeong <ritajeong.dev@gmail.com>
2022-06-06 15:04:35 +0900

    Render markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,10 +9,11 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
-
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
@@ -22,7 +23,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData
